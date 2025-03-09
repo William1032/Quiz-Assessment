6e2eba25c696c7f29d1102b2b99ac19862cd9ff0
William <william.gutsell@horowhenua.school.nz>
2025-03-10 09:44:56 +1300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 
 class Quiz():
@@ -6,43 +7,43 @@ class Quiz():
     def __init__(self):
         
 
-        #calc list
+        #previous list
         self.question_history = []
         """
-        temperature conversion gui
+        quiz gui
         """
         #window
-        self.temp_frame = Frame(padx=10, pady=10)
-        self.temp_frame.grid()
+        self.quiz_frame = Frame(padx=10, pady=10)
+        self.quiz_frame.grid()
 
         #heading
-        self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "16", "bold"))
-        self.temp_heading.grid(row=0)
+        self.quiz_heading = Label(self.quiz_frame, text="Temperature Converter", font=("Arial", "16", "bold"))
+        self.quiz_heading.grid(row=0)
 
         #instructions
-        instructions = ("Please enter a temperature below and press one of the buttons to convert it between Celcius and Fahrenheit")
-        self.temp_instructions = Label(self.temp_frame, text=instructions, wraplength=250, width=40, justify="left")
-        self.temp_instructions.grid(row=1)
+        instructions = ("Please enter an answer to the presented question")
+        self.quiz_instructions = Label(self.quiz_frame, text=instructions, wraplength=250, width=40, justify="left")
+        self.quiz_instructions.grid(row=1)
 
         #entry field
-        self.temp_entry = Entry(self.temp_frame, font=("Arial", "14"))
-        self.temp_entry.grid(row=2, padx=10, pady=10)
+        self.quiz_entry = Entry(self.quiz_frame, font=("Arial", "14"))
+        self.quiz_entry.grid(row=2, padx=10, pady=10)
 
         #errors
-        error = "Please enter a valid number"
-        self.answer_error = Label(self.temp_frame, text=error, fg="#9C0000", font=("Arial", "14", "bold"))
+        error = ""
+        self.answer_error = Label(self.quiz_frame, text=error, fg="#9C0000", font=("Arial", "14", "bold"))
         self.answer_error.grid(row=3)
 
         #conversion, help, and history
-        self.button_frame = Frame(self.temp_frame)
+        self.button_frame = Frame(self.quiz_frame)
         self.button_frame.grid(row=4)
 
         #button list (text/bg color/command/row/column)
         button_details_list = [
-            ["To Celsius", "#990099", lambda:self.check_temp(c.ABS_ZERO_FAHRENHEIT), 0, 0],
-            ["To Fahrenheit", "#009900", lambda:self.check_temp(c.ABS_ZERO_CELSIUS), 0, 1],
-            ["Info / Help", "#CC6600", "", 1, 0],
-            ["History / Export", "#004C99", lambda:self.history(self.all_calculations_list), 1, 1]
+            ["Submit Answer", "#990099", lambda:self.check_input(), 0, 1],
+            ["To Fahrenheit", "#009900", "", 1, 0],
+            ["Info / Help", "#CC6600", "", 1, 1],
+            ["History / Export", "#004C99", lambda:self.history(self.all_calculations_list), 1, 2]
         ]
 
         #hold the buttons
@@ -61,43 +62,62 @@ class Quiz():
 
     
         
-    def check_temp(self,min_temp):
+    def check_input(self):
 
         #get input
-        to_convert = self.temp_entry.get()
+        user_input = self.quiz_entry.get()
 
         #reset label and entry box in case of error
         self.answer_error.config(fg="#004C99", font=("Arial", "13", "bold"))
-        self.temp_entry.config(bg="#FFFFFF")
+        self.quiz_entry.config(bg="#FFFFFF")
 
-        error = f"Enter a number greater than/equal to {min_temp}"
+        #error = f"Enter a number greater than/equal to {min_temp}"
 
         try:
-            to_convert = float(to_convert)
-            if to_convert >= min_temp:
-                self.convert(min_temp, to_convert)
-            else:
-                self.answer_error.config(text=error, fg="#9C0000", font=("Arial", "13", "bold"))
+            user_input = float(user_input)
+            #if user_input >= min_temp:
+            self.answer_question(user_input)
+
         except ValueError:
-            self.answer_error.config(text="Temperature Invalid", fg="#9C0000", font=("Arial", "14", "bold"))
+            self.answer_error.config(text="Answer Invalid", fg="#9C0000", font=("Arial", "14", "bold"))
         
 
         #convert
-    def convert(self, min_temp, to_convert):
-        if min_temp == c.ABS_ZERO_CELSIUS:
-            answer = cr.to_fahrenheit(to_convert)
-            answer_statement = f"{to_convert} °C is {answer} °F"
-        else:
-            answer = cr.to_celsius(to_convert)
-            answer_statement = f"{to_convert} °F is {answer} °C"
 
-        self.to_history_button.config(state=NORMAL)
-        self.answer_error.config(text=answer_statement)
-        self.all_calculations_list.append(answer)
+
+    def generate_question():
+        first_part = random.randint(1, 100)
+        second_part = random.randint(1, 100)
+        operator = random.randint(1, 4)
+        if operator == 1:
+            solution = first_part + second_part
+            question_text = first_part, "+", second_part
+            instructions = ("Please enter an answer to the presented question:", question_text)
+        elif operator == 2:
+            solution = first_part - second_part
+            question_text = first_part, "-", second_part
+            instructions = ("Please enter an answer to the presented question:", question_text)
+        elif operator == 3:
+            solution = first_part / second_part
+            question_text = first_part, "/", second_part
+            instructions = ("Please enter an answer to the presented question:", question_text)
+        elif operator == 4:
+            solution = first_part * second_part
+            question_text = first_part, "x", second_part
+            instructions = ("Please enter an answer to the presented question:", question_text)
+
+
+    def answer_question(self, user_input):
+        print(user_input)
+
+
+        #self.to_history_button.config(state=NORMAL)
+        #self.answer_error.config(text=answer_statement)
+        #self.all_calculations_list.append(answer)
     
-    def history(self, list):
-        history_statement = str(list).strip("[]").replace("'", "")
-        self.answer_error.config(text=history_statement, wraplength=250)
+    #def history(self, list):
+        #history_statement = str(list).strip("[]").replace("'", "")
+        #self.answer_error.config(text=history_statement, wraplength=250)
 
 # main routine
 if __name__ == "__main__":
